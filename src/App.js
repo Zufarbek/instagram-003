edc1aae7218b8511d72db843c099436dd470b413
ALIII_077 <abdulaziz200205@icloud.com>
2022-05-07 11:10:27 +0300

    MAN OZGARTIRDMM !!!

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
        <h1>My Name ALIII_077</h1>
         <h1>MY Lastname NAJMIDDINOV</h1>
          <img width="700px" src="https://www.teahub.io/photos/full/149-1498959_free-vector-wallpaper-background-ppt-abstrak-hd.jpg" alt="ALIII_077" />
+         <h1>MY CARD</h1>
     </div>
     
   );
