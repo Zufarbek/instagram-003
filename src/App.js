feb2ca86df298c4ee6dd650bc93f02d13e482e29
ALIII_077 <abdulaziz200205@icloud.com>
2022-05-07 11:26:44 +0300

    Changed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-       <h1>My asdfasdf Name ALIII_077</h1>
+       <h1>My  Name ALIII_077</h1>
         <h1>MY Lastname NAJMIDDINOV</h1>
          <img width="700px" src="https://www.teahub.io/photos/full/149-1498959_free-vector-wallpaper-background-ppt-abstrak-hd.jpg" alt="ALIII_077" />
          <h1>MY CARD</h1>
+         <img width="700px" src="https://wroom.ru/i/news/11526img1.jpg" alt="ALIII_077" />
     </div>
     
   );
