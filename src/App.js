c6e3acfa2f259c57d8241fec2c75b2fcd940fe7d
ALIII_077 <abdulaziz200205@icloud.com>
2022-05-07 11:30:10 +0300

    my_change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
        <h1>My  Name ALIII_077</h1>
         <h1>MY Lastname NAJMIDDINOV</h1>
          <img width="700px" src="https://www.teahub.io/photos/full/149-1498959_free-vector-wallpaper-background-ppt-abstrak-hd.jpg" alt="ALIII_077" />
-         <h1>MY CARD</h1>
+         <h1>MY CARDS</h1>
          <img width="700px" src="https://wroom.ru/i/news/11526img1.jpg" alt="ALIII_077" />
     </div>
     
