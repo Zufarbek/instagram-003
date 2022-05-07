425e77f1cd04f0fe4f325fc8a527425254ada26b
ALIII_077 <abdulaziz200205@icloud.com>
2022-05-07 11:16:54 +0300

    salom

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-       <h1>My Name ALIII_077</h1>
+       <h1>My asdfasdf Name ALIII_077</h1>
         <h1>MY Lastname NAJMIDDINOV</h1>
          <img width="700px" src="https://www.teahub.io/photos/full/149-1498959_free-vector-wallpaper-background-ppt-abstrak-hd.jpg" alt="ALIII_077" />
          <h1>MY CARD</h1>
